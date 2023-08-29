c9692c183a7194871673cce79bb79a3a50d3233b
Wennie Tabib <wtabib@gmail.com>
2023-08-28 21:47:39 -0400

    Update hash for quadrotor_simulator_ros for visualization and update bag fetch url in download.py

--- data/download.py
+++ data/download.py
@@ -1,6 +1,6 @@
 import gdown
 import subprocess
 
-gdown.download_folder("https://drive.google.com/drive/folders/1q9DC6vNKfAzpLt9kNWUfWlhJqEJmBpEE?usp=share_link")
-subprocess.run("mv assignment1/*.bag .", shell=True)
-subprocess.run("rm -rf assignment1", shell=True)
+gdown.download_folder("https://drive.google.com/drive/folders/1_Iy63933LSDAFZdt9toEsdA12QsyX1QP?usp=share_link")
+subprocess.run("mv visualizer/*.bag .", shell=True)
+subprocess.run("rm -rf visualizer", shell=True)
